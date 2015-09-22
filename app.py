72b64c136f9a38abe703caf2a0aeff55b7f09ba1
AppleDash <derp@derpy.fr>
2015-09-21 20:10:04 -0400

    Add barebones Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html.jinja2")
+
+if __name__ == "__main__":
+	app.run(debug=True)

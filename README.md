0d54ab09b6178fb4118dcf2305f5ca84528953fb
AppleDash <derp@derpy.fr>
2015-09-21 20:01:09 -0400

    Remove old site code in preparation for rewrite

--- README.md
+++
@@ -1,4 +0,0 @@
-#BronyTV website 
-This is a repository to ease making changes to the BronyTV website, without needing access to the host machine.
-
-[bronytv.net](http://bronytv.net)
--- themeChanger.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	
-	$("#rariTheme1").click(function () {
-		document.body.style.background = "#202860 url('/Pictures/rariBack.png') top center no-repeat";
-		document.body.style.backgroundSize = "97% auto";
-	});
-});

672048ddb65f5ccdcfb5799879963289af8e3352
AppleDash <derp@derpy.fr>
2015-09-25 18:01:25 -0400

    nowplaying.js: Patch for proper folder

--- nowplaying.js
+++ nowplaying.js
@@ -63,7 +63,7 @@ function checkPlaylist()
   if ($('#playlist').length == 0 || !PlaylistManager) { return; }
 
   var nowplaying = $('#nowplaying').text();
-  var playlist = '/miral/nowplaying.json';
+  var playlist = '/~miralilty/nowplaying.json';
   //var isRandomPony = (nowplaying == 'Mirality - Random Pony');
   //var isSocial = (nowplaying == 'Mirality - Social Night');
   var isMirality = (nowplaying.slice(0, 10) == 'Mirality -');

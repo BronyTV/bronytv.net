d09f0fc163c07e42358d766518fe038b6f5c3ce1
AppleDash <derp@derpy.fr>
2015-10-13 22:22:59 -0400

    Improve stream page

--- static/js/angular/stream.js
+++ static/js/angular/stream.js
@@ -1,7 +1,10 @@
 var btvStreamApp = angular.module("btvStreamApp", []);
 
 btvStreamApp.controller("StreamCtrl", function($scope) {
+    $scope.chat_url = "https://kiwiirc.com/client?settings=3eda7e82f0210b259be287b5bfa1a89d";
     $scope.chatShown = true;
+    $scope.altStream = false;
+
     $scope.toggleChat = function() {
         $scope.chatShown = !$scope.chatShown;
     };
@@ -9,5 +12,13 @@ btvStreamApp.controller("StreamCtrl", function($scope) {
     $scope.reloadStream = function() {
         var $frame = $("#stream-frame");
         $frame.attr("src", $frame.attr("src"));
+    };
+
+    $scope.popoutChat = function() {
+        window.open($scope.chat_url, 'Chat', 'width=800,height=600');
+    };
+
+    $scope.swapStream = function() {
+        $scope.altStream = !$scope.altStream;
     };
 });

6f4f715c55c0c262d202d804557af74d642a0949
AppleDash <derp@derpy.fr>
2015-10-15 03:30:10 -0400

    Rip out the horribleness that was parsePost()

--- static/js/angular/index.js
+++ static/js/angular/index.js
@@ -16,40 +16,6 @@ btvIndexApp.controller('NewsCtrl', function($scope, $http) {
             $scope.news.error = true;
         });
     };
-
-    $scope.parsePost = function(post) {
-        var title;
-        var content;
-        switch (post.type) {
-            case "quote":
-                title = '<a href="' + post.short_url + '">' + post.text + '</a>';
-                content = post.source;
-                break;
-            case "photo":
-                title = '<a href="' + post.short_url + '">BronyTV</a>';
-                content = '<img src="' + post.photos[0].original_size.url + '" /><br>' + post.caption;
-                break;
-            case "link":
-                title = '<a href="' + post.url + '">' + (post.title || "Quote") + '</a>';
-                content = post.description;
-                break;
-            case "video":
-                title = '<a href="' + post.short_url + '">BronyTV</a>';
-                content = post.player[2].embed_code + '<br>' + post.caption;
-                break;
-            default:
-                title = '<a href="' + post.short_url + '">' + (post.title || "BronyTV") + '</a>';
-                content = post.body;
-                break;
-        }
-        var html = '<div class="news-post">\
-                        <h3>' + title + '</h3>\
-                        <span>' + content + '</span>\
-                    </div>\
-                    <hr>';
-
-        return html;
-    };
 });
 
 btvIndexApp.controller("CountdownCtrl", function($scope, $http) {

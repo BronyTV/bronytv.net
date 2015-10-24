e7c4ecd1212d2845534b1e93b99f9e85ac755749
AppleDash <derp@derpy.fr>
2015-10-23 20:06:22 -0400

    Finalize now streaming

--- static/js/angular/stream.js
+++ static/js/angular/stream.js
@@ -55,16 +55,17 @@ btvStreamApp.animation('.playlist-item', function() {
 });
 
 btvStreamApp.controller("StreamCtrl", function($scope, $http, $interval) {
-    $scope.properties = {stream_url: ''};
+    $scope.properties = {stream_url: '', now_streaming: null };
     $scope.chat_url = "https://kiwiirc.com/client?settings=3eda7e82f0210b259be287b5bfa1a89d";
     $scope.chatShown = true;
     $scope.altStream = false;
     $scope.showPlaylist = false;
+    $scope.streaming = false;
 
     $scope.updateValues = function() {
         $http.get("/api/properties").success(function(data) {
             $scope.properties = data["properties"];
-            console.log("Now playing: " + $scope.properties.now_streaming);
+            $scope.streaming = typeof($scope.properties.now_streaming) != "undefined" && $scope.properties.now_streaming != null && $scope.properties.now_streaming != "";
         });
 
         if ($scope.showPlaylist) {
